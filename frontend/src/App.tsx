376b45b13fdf1c24520a9da52ef924344b6929a3
Marcelo senna <marcelosennarj84@gmail.com>
2022-07-17 16:54:09 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import NotificationButton from "./components/NotificationButton"
-function App() {
+
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
+function App(): JSX.Element {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
-      <NotificationButton/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
